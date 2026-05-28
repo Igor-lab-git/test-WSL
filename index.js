b3657074c6a6d1922a600edfadbb4c449094ae88
Igor-lab-git <igorkost142@gmail.com>
2026-05-28 13:38:21 +0300

    fix-text-wsl-1

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-const j = document.querySelector("#h1")
+const j = document.querySelector("#h1");
+
+const text = j.textContent
+
+for(let i = 0; i < text.length; i++) {
+    console.log(text[i]);
+};
+
+console.log(j);
+console.log(text);
