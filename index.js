816ae1269305804c47076e461f26b4d9d4b16830
Igor-lab-git <igorkost142@gmail.com>
2026-05-29 11:54:50 +0300

    fix-29

--- index.js
+++ index.js
@@ -8,3 +8,17 @@ for(let i = 0; i < text.length; i++) {
 
 console.log(j);
 console.log(text);
+// return fetch("https://jsonplaceholder.typicode.com/todos")
+
+const get = async () => {
+    return new Promise(res => setTimeout(() => {
+         res("JENNA")
+    }, 2000))
+};
+
+const somthing = await get();
+
+console.log(somthing);
+
+
+
